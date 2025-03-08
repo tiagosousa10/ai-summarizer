908468afd180e3016b0bcf190e2480a9cd4af97c
tiagosousa <tiagosousa.tams@hotmail.com>
2025-03-08 16:55:41 +0000

    Demo input button added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,6 +17,13 @@ const Hero = () => {
           GitHub
         </button>
       </nav>
+
+      <h1 className='head_text'>
+          Summarize Articles with <br className='max-md:hidden'/> <span className='orange_gradient'>OpenAi GPT-4</span>
+      </h1>
+      <h2 className='desc'>
+          Simplify your reading with Summize, an open-source article summarizer that transforms lengthy articles into clear and concise summaries
+      </h2>
     </header>
   )
 }
