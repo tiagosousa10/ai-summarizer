b8301876fc26934ad3fe47ee860c40ac1749b262
tiagosousa <tiagosousa.tams@hotmail.com>
2025-03-08 17:58:23 +0000

    adding rapid api to article redux

--- src/services/article.js
+++ src/services/article.js
@@ -1,10 +1,23 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 
+const rapidApiKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
+
 export const articleApi = createApi({
   reducerPath: 'articleApi', // unique name 
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'article-extractor-and-summarizer.p.rapidapi.com/',
+    prepareHeaders: (headers) => {
+      headers.set('x-rapidapi-key', rapidApiKey)
+      headers.set('x-rapidapi-host', 'article-extractor-and-summarizer.p.rapidapi.com')
+
+      return headers
+    }
+  }),
   endpoints: (builder) => ({
     getSummary: builder.query({
-      query: (params) => `test`
+      query: (params) => `/summarize?url=${params.articleUrl}&length=3`
     })
   }) 
 })
+
+export const {useLazyGetSummaryQuery} = articleApi;
