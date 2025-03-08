eee398be058539646a6542ecea605fda0fe3cf00
tiagosousa <tiagosousa.tams@hotmail.com>
2025-03-08 19:09:11 +0000

    browse the url history and display the results

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -21,17 +21,17 @@ const Demo = () => {
     }
   }, [])
 
+  //----- handle submit -----
   const handleSubmit = async (e) => {
     e.preventDefault();
     const {data} = await getSummary({articleUrl: article.url})
-    console.log('cheguei aqui', data)
     
     if(data?.summary) {
       const newArticle = {...article, summary: data.summary}
       const updatedAllArticles = [newArticle, ...allArticles]
 
       setArticle(newArticle)
-      setAllArticles(updatedAllArticles)
+      setAllArticles(updatedAllArticles) 
 
       localStorage.setItem('articles', JSON.stringify(updatedAllArticles))
 
@@ -75,7 +75,10 @@ const Demo = () => {
               className='link_card'
             >
               <div className='copy_btn'>
-                <img src={copy} alt="copy_icon" className='w-[40%] h-[40%] object-contain' />
+                <img src={copy} 
+                  alt="copy_icon" 
+                  className='w-[40%] h-[40%] object-contain' 
+                />
               </div>
 
               <p className='flex-1 font-satoshi text-blue-700 font-medium text-sm truncate'>
@@ -89,7 +92,34 @@ const Demo = () => {
       </div>
 
       {/*Display Result */}
-
+      <div className='my-10 max-w-full flex justify-center items-center'>
+        {isFetching ? (
+          <img 
+            src={loader}
+            alt='loader'
+            className='w-20 h-20 object-contain'
+          />
+        ) : error ? (
+          <p className='font-inter font-bold text-black text-center'>
+            Well, that wasn't supposed to happen. 
+            <br />
+            <span className='font-satoshi font-normal text-gray-700'>
+              {error?.data?.error}
+            </span>
+          </p>
+        ) : (
+          article.summary && (
+            <div className='flex flex-col gap-3'>
+              <h2 className='font-satoshi font-bold text-gray-600'>
+                Article <span className='blue_gradient'>Summary</span>
+              </h2>
+              <div className='summary_box'>
+                <p className='font-inter font-medium text-sm text-gray-700'>{article.summary}</p>
+              </div>
+            </div>
+          )
+        )}
+      </div>
     </section>
   )
 }
